074b146833f1dfcdf8d3272433a71e0162ac3227
Sungjin Kim <jamessungjin.kim@gmail.com>
2022-12-04 23:03:48 +0900

    updated

---
+++ P1_Basic_Programming/hello.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // hello world
+    println!("Hello, world!");
+    // chage to c
+    // printf("Hello, world!\n");
+    // change to c++
+    // cout << "Hello, world!" << endl;
+
+    // No \n
+    print!("Hello, world!\n");
+}
