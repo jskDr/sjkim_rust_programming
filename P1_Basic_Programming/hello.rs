9a765c485afa7b64554a031046997e6fff10c7cd
Sungjin Kim <jamessungjin.kim@gmail.com>
2022-12-05 01:44:41 +0900

    hello

--- P1_Basic_Programming/hello.rs
+++ P1_Basic_Programming/hello.rs
@@ -5,6 +5,24 @@ fn main() {
     // printf("Hello, world!\n");
     // change to c++
     // cout << "Hello, world!" << endl;
+    // change to java
+    // System.out.println("Hello, world!");
+    // change to python
+    // print("Hello, world!")
+    // change to ruby
+    // puts "Hello, world!"
+    // change to perl
+    // print "Hello, world!
+    // change to php
+    // echo "Hello, world!
+    // change to lua
+    // print("Hello, world!")
+    // change to javascript
+    // console.log("Hello, world!")
+    // change to go
+    // fmt.Println("Hello, world!")
+    // change to typescript
+    // console.log("Hello, world!")
 
     // No \n
     print!("Hello, world!\n");
