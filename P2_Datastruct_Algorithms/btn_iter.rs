2e31290cd95d7a74d93735bab4fe8cd2fb225153
Sungjin Kim <jamessungjin.kim@gmail.com>
2022-12-10 22:48:59 +0900

    modify

--- P2_Datastruct_Algorithms/btn_iter.rs
+++ P2_Datastruct_Algorithms/btn_iter.rs
@@ -1,3 +1,6 @@
+// import vecdeque
+use std::collections::VecDeque;
+
 // Binary Tree Node
 struct BTN {
     data: i32, // i8, i16, i32, i64, i128, isize
@@ -7,17 +10,45 @@ struct BTN {
 
 // implementation for BTN
 impl BTN {
+    fn new(data: i32) -> BTN {
+        BTN {
+            data,
+            left: None,
+            right: None
+        }
+    }
 }
 
 fn print_preorder(root: &BTN) {
     let mut queue: VecDeque<&BTN> = VecDeque::new();
+    queue.push_back(root); // &BTN(1, None, None)
+    while !queue.is_empty() { // 1,
+        let mut node = queue.pop_front().unwrap(); // node = BTN(1, S(B(2)), S(B(3)))
+        print!("{} ", node.data); // 1,        
+        while let Some(left) = &node.left { // left = &B(2,3,4), &B(3,4,5)
+            queue.push_back(left); // &B(2,3,4)
+            node = left.as_ref();  // node = &B(2,3,4)
+            println!("{} ", node.data); // 2,
+            println!("{} ", node.left.as_ref().unwrap().data); // 3,
+        }
+        if let Some(right) = &node.right {
+            queue.push_back(right);
+        }
+    }
     println!();
 }
 
 fn main() {
-    today_tip();
+    // today_tip();
 
     println!("Preorder of BTN");
+    let mut root = BTN::new(1);
+    let left = BTN::new(2);
+    let right = BTN::new(3);
+    let left_left = BTN::new(4);
+    let left_right = BTN::new(5);
+    let right_left = BTN::new(6);
+    let right_right = BTN::new(7);
  
     /*
           1
@@ -26,12 +57,22 @@ fn main() {
       / \   /  \ 
      4   5 6    7
     */
+    root.left = Some(Box::new(left)); 
+    root.right = Some(Box::new(right));
+    if let Some(left) = &mut root.left {
+        left.left = Some(Box::new(left_left));
+        left.right = Some(Box::new(left_right));
+    }
+    if let Some(right) = &mut root.right {
+        right.left = Some(Box::new(right_left));
+        right.right = Some(Box::new(right_right));
+    }
   
     print_preorder(&root);
 }
 
 
-fn today_tip() {
+fn _today_tip() {
     println!("Today's tip: if let");
     let i = Some(1);
     if let Some(j) = &i {
