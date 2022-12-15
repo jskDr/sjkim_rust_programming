87121c67fe2c3e01cd07a583e86336a7811431ce
jskdr <jamessungjin.kim@gmail.com>
2022-12-15 23:31:23 +0900

    testing

--- P2_Datastruct_Algorithms/07_subsum_recursive.rs
+++ P2_Datastruct_Algorithms/07_subsum_recursive.rs
@@ -8,6 +8,6 @@ fn test_subsum(set: &[u32], sum: u32) {
 
 fn main() {
     let set: [u32;6] = [3, 34, 4, 12, 5, 2];
-    test_subsum(&set, 9);
-    test_subsum(&set, 30);
+    // test_subsum with sum = 9
+    // test_subsum with sum = 30
 }
