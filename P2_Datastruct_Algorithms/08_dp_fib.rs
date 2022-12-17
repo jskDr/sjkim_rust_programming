3b8994783c0bf0f50a6f3df7904f8c06d387d88a
Sungjin Kim <jamessungjin.kim@gmail.com>
2022-12-17 21:33:13 +0900

    update

--- P2_Datastruct_Algorithms/08_dp_fib.rs
+++ P2_Datastruct_Algorithms/08_dp_fib.rs
@@ -1,10 +1,18 @@
 fn fib_rec(n: u32) -> u32 {
-    0
+    if n == 0 {0}
+    else if n == 1 {1}
+    else {fib_rec(n-1) + fib_rec(n-2)}
 }
 
 fn fib_dp_tab(n: u32) -> u32 { // n: 10
     // tabulation dynamic programming for fibonacci sequence
-    0
+    let mut tab: Vec<u32> = Vec::new(); // vec![u32;n as usize + 1]
+    tab.push(0);
+    tab.push(1);
+    for i in 2..n as usize + 1 {
+        tab.push( tab[i-1] + tab[i-2]);
+    }
+    tab[n as usize] // indexing should be usize
 }
 
 struct Fib {
@@ -16,7 +24,12 @@ impl Fib {
         Fib { memo: vec![0; max_n as usize] }
     }
     fn calc(&mut self, n: u32) -> u32 {
-        0
+        if self.memo[n as usize] != 0 {
+            return self.memo[n as usize];
+        }
+        if n == 1 {self.memo[1] = 1}
+        if n > 1 {self.memo[n as usize] = self.calc(n-1) + self.calc(n-2)}
+        self.memo[n as usize]
     }    
 }
 
