6ae3e2f2914baf7807e723798ec610c900825d2c
jskdr <jamessungjin.kim@gmail.com>
2022-12-17 22:37:31 +0900

    option type

--- P2_Datastruct_Algorithms/08_dp_fib_org_r0.rs
+++ P2_Datastruct_Algorithms/08_dp_fib_org_r0.rs
@@ -17,22 +17,19 @@ fn fib_dp_tab(n: u32) -> u32 { // n: 10
 }
 
 struct Fib {
-    memo: Vec<Option<u32>>,
+    memo: Vec<u32>,
 }
 
 impl Fib {
     fn new(max_n: u32) -> Fib {
-        Fib { memo: vec![None; max_n as usize] }
+        Fib { memo: vec![0; max_n as usize] }
     }
     fn calc(&mut self, n: u32) -> u32 {
-        if let Some(x) = self.memo[n as usize] { 
-            x
-        } else {      
-            if n == 0 { self.memo[n as usize] = Some(0) }  
-            else if n == 1 { self.memo[n as usize] = Some(1) }
-            else { self.memo[n as usize] = Some(self.calc(n-1) + self.calc(n-2)) }
-            self.memo[n as usize].unwrap()
-        }
+        if n == 0 { return 0; }
+        if n == 1 { return 1; }
+        if self.memo[n as usize] != 0 { return self.memo[n as usize]; }
+        self.memo[n as usize] = self.calc(n-1) + self.calc(n-2);
+        self.memo[n as usize]
     }    
 }
 
