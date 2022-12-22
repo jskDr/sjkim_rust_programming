bf641c8725157f09a2d74561c58474cffd90946c
Sungjin Kim <jamessungjin.kim@gmail.com>
2022-12-22 22:27:08 +0900

    rename

--- P2_Datastruct_Algorithms/09_dp_subsum.rs
+++ P2_Datastruct_Algorithms/09_dp_subsum.rs
@@ -1,10 +1,14 @@
 fn check_subsum_rec(set: &Vec<u32>, sum: u32, n: usize) -> bool {
-    true
+    if sum == 0 { true }
+    else if n == 0 { false }
+    else if set[n-1] > sum { check_subsum_rec(&set, sum, n-1) }
+    else { check_subsum_rec(&set, sum, n-1) || 
+        check_subsum_rec(&set, sum - set[n-1], n-1)}
 }
 
 fn check_subsum_dp_tab(set: &Vec<u32>, sum: u32, n: usize) -> bool {
     true
-  }
+}
 
 fn test_subsum(set: &Vec<u32>, sum: u32) {
     println!("Example: set = {:?} and sum = {}", set, sum);
