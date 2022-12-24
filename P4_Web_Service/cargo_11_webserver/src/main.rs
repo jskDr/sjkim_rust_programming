638b79312cc9efd009db2348d89fccd41904cd3e
Sungjin Kim <jamessungjin.kim@gmail.com>
2022-12-25 01:05:26 +0900

    mac

--- P4_Web_Service/cargo_11_webserver/src/main.rs
+++ P4_Web_Service/cargo_11_webserver/src/main.rs
@@ -5,5 +5,21 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("localhost:7777").unwrap();
+    // let listener = TcpListener::bind("localhost:7777").unwrap();
+    let listener = match TcpListener::bind("localhost:8888") {
+        Ok(listener) => listener,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    for stream in listener.incoming() {
+        webcomm(stream.unwrap());
+    }
+}
+
+fn webcomm(mut stream: TcpStream) {
+    let content = fs::read_to_string("html/hello.html").unwrap();
+    let length = content.len();
+    let prefix = format!("HTTP1.2 Ok\r\nContent-Length {length}\r\n\r\n");
+    let output = format!("{prefix}{content}");
+    stream.write(output.as_bytes()).unwrap();
 }
